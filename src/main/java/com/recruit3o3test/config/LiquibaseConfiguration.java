15df8ba6f0b9719c286638fd8c7857baff696aec
yorez <yorez@naver.com>
2022-01-07 23:43:28 +0900

    feat: Liquibase - add default dependency and layout

---
+++ src/main/java/com/recruit3o3test/config/LiquibaseConfiguration.java
@@ -0,0 +1,38 @@
+package com.recruit3o3test.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+    private final Environment env;
+    public LiquibaseConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource()  {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+        liquibase.setDataSource(dataSource());
+        return liquibase;
+    }
+}
