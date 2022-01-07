91d677723bc017a6897b8aded223892fa540af78
yorez <yorez@naver.com>
2022-01-08 01:37:08 +0900

    feat: DatabaseConfiguration.java - add default layout

---
+++ src/main/java/com/recruit3o3test/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,16 @@
+package com.recruit3o3test.security;
+
+import com.recruit3o3test.config.Constants;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(Constants.SYSTEM);
+    }
+}
