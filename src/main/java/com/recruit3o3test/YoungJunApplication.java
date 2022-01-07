5edaabf9d20fc24ae59432aebfcf8923bedbe3dc
yzero <yorez@naver.com>
2022-01-07 21:40:14 +0900

    first commit

---
+++ src/main/java/com/recruit3o3test/YoungJunApplication.java
@@ -0,0 +1,13 @@
+package com.recruit3o3test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungJunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungJunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruit3o3test/YoungJunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruit3o3test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungJunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
