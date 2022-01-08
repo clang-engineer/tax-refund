ef3dc4fcc1797040cf13f9b5f6e884b7272f9ebd
yorez <yorez@naver.com>
2022-01-08 15:40:58 +0900

    refactor: TaxRefundApplicationTests.java - extract @IntegrationTest

--- src/test/java/com/szs/web/UserResourceIT.java
+++ src/test/java/com/szs/web/UserResourceIT.java
@@ -1,22 +1,22 @@
 package com.szs.web;
 
-import com.szs.TaxRefundApplication;
+import com.szs.IntegrationTest;
 import com.szs.domain.User;
 import com.szs.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = TaxRefundApplication.class)
+
 @AutoConfigureMockMvc
+@IntegrationTest
 public class UserResourceIT {
 
     private static final String DEFAULT_USER_ID = "AAAAAAAAA";
