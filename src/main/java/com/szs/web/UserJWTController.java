883b1b3033ff1b37d640f38d3b5ffcf5002d2bfe
yorez <yorez@naver.com>
2022-01-08 17:57:45 +0900

    modify: UserJWTController.java - set jwt cookie on browser

--- src/main/java/com/szs/web/UserJWTController.java
+++ src/main/java/com/szs/web/UserJWTController.java
@@ -1,6 +1,7 @@
 package com.szs.web;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.szs.config.ApplicationProperties;
 import com.szs.security.jwt.JWTFilter;
 import com.szs.security.jwt.TokenProvider;
 import com.szs.web.vm.LoginVM;
@@ -16,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/api")
@@ -26,13 +30,16 @@ public class UserJWTController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public UserJWTController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
+    private final ApplicationProperties applicationProperties;
+
+    public UserJWTController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder, ApplicationProperties applicationProperties) {
         this.tokenProvider = tokenProvider;
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.applicationProperties = applicationProperties;
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<JWTToken> authorize(@Valid @RequestBody LoginVM loginVM) {
+    public ResponseEntity<JWTToken> authorize(@Valid @RequestBody LoginVM loginVM, HttpServletResponse response) throws Exception {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginVM.getUserId(), loginVM.getPassword()
         );
@@ -41,6 +48,14 @@ public class UserJWTController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String jwt = tokenProvider.createToken(authentication);
+
+        Cookie cookie = CookieUtils.createCookie(
+                "Authorization",
+                URLEncoder.encode("Bearer " + jwt, "UTF-8"),
+                (int) applicationProperties.getSecurity().getAuthentication().getJwt().getTokenValidityInSeconds());
+
+        response.addCookie(cookie);
+
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
         return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
