eb69279b32577e0e126ab78529eb126b9da55673
yorez <yorez@naver.com>
2022-01-08 15:43:59 +0900

    modify: SecurityConfiguration.java - add BCryptPasswordEncoder

--- src/main/java/com/szs/config/SecurityConfiguration.java
+++ src/main/java/com/szs/config/SecurityConfiguration.java
@@ -1,14 +1,22 @@
 package com.szs.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public void configure(WebSecurity web) {
         web
@@ -25,13 +33,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .csrf()
                 .disable()
-                .formLogin()
-                .loginProcessingUrl("/api/authentication")
-                .and()
-                .logout()
-                .logoutUrl("/api/logout")
-                .and()
                 .authorizeRequests()
+                .antMatchers("/api/authenticate").permitAll()
                 .antMatchers("/api/users").permitAll()
                 .antMatchers("/login*").permitAll()
                 .antMatchers("/api/**").authenticated()
