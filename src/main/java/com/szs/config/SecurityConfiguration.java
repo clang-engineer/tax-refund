aae6e3003c0195c039794c0652b6b61ec1d23103
yorez <yorez@naver.com>
2022-01-08 14:54:27 +0900

    feat: SecurityConfiguration.java - add spring security module

---
+++ src/main/java/com/szs/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.szs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**")
+                .antMatchers("/v3/api-docs")
+                .antMatchers("/swagger-resources/**")
+                .antMatchers("/swagger-ui/**")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .formLogin()
+                .loginProcessingUrl("/api/authentication")
+                .and()
+                .logout()
+                .logoutUrl("/api/logout")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/users").permitAll()
+                .antMatchers("/login*").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .and()
+                .httpBasic();
+    }
+}
