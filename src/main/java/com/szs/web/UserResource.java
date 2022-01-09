e0d4d73159e36728d351a24580e5419234506e2d
yorez <yorez@naver.com>
2022-01-09 22:39:43 +0900

    fix: UserResource.java - add password encode logic

--- src/main/java/com/szs/web/UserResource.java
+++ src/main/java/com/szs/web/UserResource.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,10 +25,15 @@ public class UserResource {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     private final ScrapService scrapService;
 
-    public UserResource(UserRepository userRepository, ScrapService scrapService) {
+    public UserResource(UserRepository userRepository,
+                        PasswordEncoder passwordEncoder,
+                        ScrapService scrapService) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
         this.scrapService = scrapService;
     }
 
@@ -41,6 +47,7 @@ public class UserResource {
 
         User user = new User(userDTO);
 
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRegNo(AES256Utils.encrypt(user.getRegNo()));
         User result = userRepository.save(user);
 
