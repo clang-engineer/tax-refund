b7ab35578a01dfca8f77d1f8a09e3b9a65fe4d7a
yorez <yorez@naver.com>
2022-01-09 14:59:39 +0900

    refactor: ScrapService.java - extract transform external json method

--- src/main/java/com/szs/service/ScrapService.java
+++ src/main/java/com/szs/service/ScrapService.java
@@ -49,24 +49,32 @@ public class ScrapService {
 
 
     public Optional<Scrap> saveScrapInfo(String name, String regNo) {
+        log.debug("Save current user scrap info from external network");
+
         Map map = Map.of("name", name, "regNo", regNo);
         HashMap externalScrapInfo = (HashMap) this.restTemplate.postForObject("https://codetest.3o3.co.kr/scrap/", map, Map.class);
 
-        HashMap jsonList = (HashMap) externalScrapInfo.get("jsonList");
+        Scrap result = transformJsonToScarp(externalScrapInfo);
+        scrapRepository.save(result);
+        return Optional.of(result);
+    }
+
+    private Scrap transformJsonToScarp(HashMap externalJson) {
+        HashMap jsonList = (HashMap) externalJson.get("jsonList");
         Map scrap001 = (HashMap) ((ArrayList) jsonList.get("scrap001")).get(0);
         Map scrap002 = (HashMap) ((ArrayList) jsonList.get("scrap002")).get(0);
         Scrap result = new Scrap()
-                .appVer((String) externalScrapInfo.get("appVer"))
-                .hostNm((String) externalScrapInfo.get("hostNm"))
-                .workerResDt(LocalDateTime.parse((String) externalScrapInfo.get("workerResDt")))
-                .workerReqDt(LocalDateTime.parse((String) externalScrapInfo.get("workerReqDt")))
+                .appVer((String) externalJson.get("appVer"))
+                .hostNm((String) externalJson.get("hostNm"))
+                .workerResDt(LocalDateTime.parse((String) externalJson.get("workerResDt")))
+                .workerReqDt(LocalDateTime.parse((String) externalJson.get("workerReqDt")))
                 .errMsg((String) jsonList.get("errMsg"))
                 .company((String) jsonList.get("company"))
                 .svcCd((String) jsonList.get("svcCd"))
                 .userId((String) jsonList.get("userId"))
                 .scrap001(scrap001)
                 .scrap002(scrap002);
-        scrapRepository.save(result);
-        return Optional.of(result);
+
+        return result;
     }
 }
