bac0ecde6e9a53b439bf72483c147a5ed89b4e9f
yorez <yorez@naver.com>
2022-01-11 10:23:59 +0900

    refactor: RefundUtils.java  - extract method

--- src/main/java/com/szs/web/RefundController.java
+++ src/main/java/com/szs/web/RefundController.java
@@ -1,6 +1,5 @@
 package com.szs.web;
 
-import com.szs.config.Constants;
 import com.szs.domain.ScrapSalary;
 import com.szs.domain.ScrapTax;
 import com.szs.domain.User;
@@ -48,58 +47,15 @@ public class RefundController {
 
         ScrapDTO scrap = scrapService.getScrapInfo().orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
 
-        int limitedMoney = getLimitedMoney(scrap.getScrapSalaryList().stream().map(ScrapSalary::getTotal).reduce(0, Integer::sum));
-        int deductedMoney = getDeductedMoney(scrap.getScrapTaxList().stream().map(ScrapTax::getTotal).reduce(0, Integer::sum));
+        int limitedMoney = RefundUtils.getLimitedMoney(scrap.getScrapSalaryList().stream().map(ScrapSalary::getTotal).reduce(0, Integer::sum));
+        int deductedMoney = RefundUtils.getDeductedMoney(scrap.getScrapTaxList().stream().map(ScrapTax::getTotal).reduce(0, Integer::sum));
         int refundMoney = Math.min(limitedMoney, deductedMoney);
 
-        result.put("한도", getFormattedMoney(limitedMoney));
-        result.put("공제액", getFormattedMoney(deductedMoney));
-        result.put("환급액", getFormattedMoney(refundMoney));
+        result.put("한도", RefundUtils.getFormattedMoney(limitedMoney));
+        result.put("공제액", RefundUtils.getFormattedMoney(deductedMoney));
+        result.put("환급액", RefundUtils.getFormattedMoney(refundMoney));
 
         return ResponseEntity.ok(result);
-    }
-
-    public Integer getLimitedMoney(Integer totalSalary) {
-        if (totalSalary <= Constants.REFUND_LOWER_LIMIT) {
-            return 740000;
-        } else if (totalSalary <= Constants.REFUND_UPPER_LIMIT) {
-            Integer temp = (int) (740000 - (totalSalary - Constants.REFUND_LOWER_LIMIT) * 0.008);
-            return Math.max(660000, temp);
-        } else {
-            Integer temp = (int) (660000 - (totalSalary - Constants.REFUND_UPPER_LIMIT) * 0.5);
-            return Math.max(500000, temp);
-        }
-    }
-
-    public Integer getDeductedMoney(Integer calculateTax) {
-        Integer standardMoney = 1300000;
-        if (calculateTax <= standardMoney) {
-            return (int) (calculateTax * 0.55);
-        } else {
-            return (int) (715000 + (calculateTax - standardMoney) * 0.3);
-        }
-    }
-
-    public String getFormattedMoney(Integer money) {
-        int first = money / 10000;
-        int second = money % 10000 / 1000;
-
-        String result = "";
-        if (first != 0) {
-            result += first + "만";
-        }
-
-        if (second != 0) {
-            if (first != 0) {
-                result += " ";
-            }
-            result += second + "천";
-        }
-
-        result += "원";
-
-        return result;
-
     }
 
 }
---
+++ src/main/java/com/szs/web/RefundUtils.java
@@ -0,0 +1,51 @@
+package com.szs.web;
+
+import com.szs.config.Constants;
+
+public class RefundUtils {
+    public static Integer getLimitedMoney(Integer totalSalary) {
+        if (totalSalary <= Constants.REFUND_LOWER_LIMIT) {
+            return 740000;
+        } else if (totalSalary <= Constants.REFUND_UPPER_LIMIT) {
+            Integer temp = (int) (740000 - (totalSalary - Constants.REFUND_LOWER_LIMIT) * 0.008);
+            return Math.max(660000, temp);
+        } else {
+            Integer temp = (int) (660000 - (totalSalary - Constants.REFUND_UPPER_LIMIT) * 0.5);
+            return Math.max(500000, temp);
+        }
+    }
+
+    public static Integer getDeductedMoney(Integer calculateTax) {
+        Integer standardMoney = 1300000;
+        if (calculateTax <= standardMoney) {
+            return (int) (calculateTax * 0.55);
+        } else {
+            return (int) (715000 + (calculateTax - standardMoney) * 0.3);
+        }
+    }
+
+    public static String getFormattedMoney(Integer money) {
+        if (money < 1000) {
+            return "0원";
+        }
+        int first = money / 10000;
+        int second = money % 10000 / 1000;
+
+        String result = "";
+        if (first != 0) {
+            result += first + "만";
+        }
+
+        if (second != 0) {
+            if (first != 0) {
+                result += " ";
+            }
+            result += second + "천";
+        }
+
+        result += "원";
+
+        return result;
+    }
+
+}
