4b5b3d317ac18dbdd8e4f83c96243eee74a2547c
yorez <yorez@naver.com>
2022-01-13 09:11:07 +0900

    refactor: Constants.java - naming rule

--- src/test/java/com/szs/service/RefundServiceUnitTest.java
+++ src/test/java/com/szs/service/RefundServiceUnitTest.java
@@ -17,19 +17,19 @@ public class RefundServiceUnitTest {
 
     @Test
     void testGetLimitedMoneyLower() {
-        assertThat(refundService.getLimitedMoney(Constants.REFUND_LOWER_LIMIT)).isEqualTo(740000);
-        assertThat(refundService.getLimitedMoney(Constants.REFUND_LOWER_LIMIT - 1)).isEqualTo(740000);
-        assertThat(refundService.getLimitedMoney(Constants.REFUND_LOWER_LIMIT + 1)).isNotEqualTo(740000);
+        assertThat(refundService.getLimitedMoney(Constants.SALARY_LOWER_LIMIT)).isEqualTo(740000);
+        assertThat(refundService.getLimitedMoney(Constants.SALARY_LOWER_LIMIT - 1)).isEqualTo(740000);
+        assertThat(refundService.getLimitedMoney(Constants.SALARY_LOWER_LIMIT + 1)).isNotEqualTo(740000);
     }
 
     @Test
     void testGetLimitedMoneyMiddle() {
-        int totalSalary1 = Constants.REFUND_LOWER_LIMIT + 1;
+        int totalSalary1 = Constants.SALARY_LOWER_LIMIT + 1;
         int result = (int) (740000 - (totalSalary1 - 33000000) * 0.008);
         result = Math.max(result, 660000);
         assertThat(refundService.getLimitedMoney(totalSalary1)).isEqualTo(result);
 
-        int totalSalary2 = Constants.REFUND_UPPER_LIMIT;
+        int totalSalary2 = Constants.SALARY_UPPER_LIMIT;
         int result2 = (int) (740000 - (totalSalary2 - 33000000) * 0.008);
         result2 = Math.max(result2, 660000);
         assertThat(refundService.getLimitedMoney(totalSalary2)).isEqualTo(result2);
@@ -37,7 +37,7 @@ public class RefundServiceUnitTest {
 
     @Test
     void testGetLimitedMoneyUpper() {
-        int totalSalary1 = Constants.REFUND_UPPER_LIMIT + 1;
+        int totalSalary1 = Constants.SALARY_UPPER_LIMIT + 1;
         int result = (int) (660000 - (totalSalary1 - 70000000) * 0.5);
         assertThat(refundService.getLimitedMoney(totalSalary1)).isEqualTo(result);
     }
