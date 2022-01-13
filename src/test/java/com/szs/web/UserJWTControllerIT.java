f1391b21bce4a1548c7e03cefd8072e771f0c271
yorez <yorez@naver.com>
2022-01-13 12:55:09 +0900

    chore: clean up

--- src/test/java/com/szs/web/UserJWTControllerIT.java
+++ src/test/java/com/szs/web/UserJWTControllerIT.java
@@ -4,6 +4,8 @@ import com.szs.IntegrationTest;
 import com.szs.domain.User;
 import com.szs.repository.UserRepository;
 import com.szs.web.vm.LoginVM;
+import com.szs.web.vm.ManagedUserVM;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -29,21 +31,26 @@ class UserJWTControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
+    private User user;
+
+    @BeforeEach
+    void initTest() {
+        user = UserResourceIT.createEntity();
+    }
+
     @Test
     @Transactional
     void testAuthorize() throws Exception {
-        User user = new User()
-                .useId("testuserid")
-                .name("testName")
-                .regNo("testRegNo")
-                .password(passwordEncoder.encode("test1234"));
-
+        //given
+        user.setUserId("test_id");
+        user.setPassword(passwordEncoder.encode("test1234"));
         userRepository.saveAndFlush(user);
 
-        LoginVM login = new LoginVM();
-        login.setUserId("testuserid");
+        ManagedUserVM login = new ManagedUserVM();
+        login.setUserId("test_id");
         login.setPassword("test1234");
 
+        //when, then
         mockMvc
                 .perform(post("/szs/login")
                         .contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(login)))
@@ -51,15 +58,18 @@ class UserJWTControllerIT {
                 .andExpect(jsonPath("$.id_token").isString())
                 .andExpect(jsonPath("$.id_token").isNotEmpty())
                 .andExpect(header().string("Authorization", not(nullValue())))
-                .andExpect(header().string("Authorization", not(is(emptyString())))) ;
+                .andExpect(header().string("Authorization", not(is(emptyString()))));
     }
 
     @Test
     @Transactional
     void testAuthorizeFails() throws Exception {
+        //given
         LoginVM login = new LoginVM();
         login.setUserId("wrong-userid");
         login.setPassword("wrong password");
+
+        //when, then
         mockMvc
                 .perform(post("/szs/login").contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(login)))
                 .andExpect(status().isUnauthorized())
